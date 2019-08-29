4538d6fc031384c5144325b33ff1fa1ec19e109c
Michael Santos <michael.shel96@gmail.com>
2019-08-29 09:47:24 -0300

    Implementação de seleções para funcionamento da class

--- ponto/src/main/java/br/com/msantos/ponto/repository/FuncionarioRepository.java
+++ ponto/src/main/java/br/com/msantos/ponto/repository/FuncionarioRepository.java
@@ -1,10 +1,12 @@
 package br.com.msantos.ponto.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.msantos.ponto.models.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
 
-	public Funcionario findByCpf(String cpf); 
+	public Optional<Funcionario> findByCpf(String cpf); 
 }
--- ponto/src/main/java/br/com/msantos/ponto/repository/PontoEletronicoRepository.java
+++ ponto/src/main/java/br/com/msantos/ponto/repository/PontoEletronicoRepository.java
@@ -1,10 +1,25 @@
 package br.com.msantos.ponto.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Set;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.msantos.ponto.models.Funcionario;
 import br.com.msantos.ponto.models.PontoEletronico;
 
 public interface PontoEletronicoRepository extends JpaRepository<PontoEletronico, Long> {
 
+	@Query("select p from PontoEletronico p where p.funcionario = :funcionario and p.diaEncerrado is false and p.justificativa is false")
+	Optional<PontoEletronico> findByPontoAtualDoFuncionario(@Param("funcionario") Funcionario funcionario);
+	
+	@Query("select p from PontoEletronico p where p.funcionario = :funcionario "
+			+ "and p.inicioJornada between :inicio and :termino order by p.inicioJornada")
+	Set<PontoEletronico> findByPontoAndPeriodo(@Param("funcionario") Funcionario funcionario,
+				@Param("inicio") LocalDateTime inicio,
+				@Param("termino") LocalDateTime termino);
 	
 }
