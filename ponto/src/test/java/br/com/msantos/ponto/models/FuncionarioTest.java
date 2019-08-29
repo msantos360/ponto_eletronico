35ab0517021e745217071be5408e16ea9607f506
Michael Santos <michael.shel96@gmail.com>
2019-08-29 09:50:03 -0300

    implementação de novos testes

--- ponto/src/test/java/br/com/msantos/ponto/models/FuncionarioTest.java
+++ ponto/src/test/java/br/com/msantos/ponto/models/FuncionarioTest.java
@@ -1,6 +1,7 @@
 package br.com.msantos.ponto.models;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,33 +17,29 @@ import br.com.msantos.ponto.repository.PontoEletronicoRepository;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FuncionarioTest {
-	
+
 	@Autowired
 	JornadaTrabalhoRepository jornadaTrabalhoRepository;
-	
+
 	@Autowired
 	FuncionarioRepository funcionarioRepository;
-	
+
 	@Autowired
 	PontoEletronicoRepository pontoEletronicoRepository;
-	
+
 	private JornadaTrabalho jornadaTrabalho = null;
-	
+
 	@Before
 	public void criaJornadaDeTrabalho() {
-		jornadaTrabalho = new JornadaTrabalhoBuilder()
-			.comInicioJornada(8, 00)
-			.comTerminoJornada(17, 00)
-			.comTipoTrabalhador(TipoTrabalhador.RURAL)
-			.comCargaHorariaSemanal(36)
-			.comSabado()
-			.builder();
+		jornadaTrabalho = new JornadaTrabalhoBuilder().comInicioJornada(8, 00).comTerminoJornada(17, 00)
+				.comTipoTrabalhador(TipoTrabalhador.RURAL).comCargaHorariaSemanal(36).comSabado().builder();
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void naoDevePermiteCpfIncorreto() {
 
-		new Funcionario(jornadaTrabalho, "43865969097", "Paulo", LocalDate.now(), Sexo.M, "2569-9856", "teste@teste.com", true);
+		new Funcionario(jornadaTrabalho, "43865969097", "Paulo", LocalDate.now(), Sexo.M, "2569-9856",
+				"teste@teste.com", true);
 
 	}
 
@@ -57,64 +54,53 @@ public class FuncionarioTest {
 	@Test
 	public void retiraPontuacaoDeCpf() {
 		LocalDate dataNascimento = LocalDate.of(2000, 8, 23);
-		new Funcionario(jornadaTrabalho, "438.659.690-97", "Paulo", dataNascimento, Sexo.M, "2569-9856", "teste@teste.com", true);
+		new Funcionario(jornadaTrabalho, "438.659.690-97", "Paulo", dataNascimento, Sexo.M, "2569-9856",
+				"teste@teste.com", true);
 	}
 
 	@Test
 	public void funcionarioComFuncionarioBuider() {
-		new FuncionarioBuilder()
-			.comCpf("43865969097")
-			.comDataNascimento(20,2,2000)
-			.comSexo(Sexo.M)
-			.comTelefone("2659-0000")
-			.comJornadaDeTrabalho(jornadaTrabalho)
-			.comEmail("teste@teste.com")
-			.estaAtivo(true)
-			.comNome("Paulo").builder();
-		
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void exigeCamposRequeridosCpfNomeDataNascimentoEsexo() {
-		
-		new FuncionarioBuilder()
-			.comDataNascimento(20,2,2000)
-			.comSexo(Sexo.M)
-			.comTelefone("2659-0000")
-			.comJornadaDeTrabalho(jornadaTrabalho)
-			.comEmail("teste@teste.com")
-			.estaAtivo(true)
-			.comNome("Paulo").builder();
+		new FuncionarioBuilder().comCpf("43865969097").comDataNascimento(20, 2, 2000).comSexo(Sexo.M)
+				.comTelefone("2659-0000").comJornadaDeTrabalho(jornadaTrabalho).comEmail("teste@teste.com")
+				.estaAtivo(true).comNome("Paulo").builder();
 
 	}
-	
+
+	@Test(expected = IllegalArgumentException.class)
+	public void exigeCamposRequeridosCpfNomeDataNascimentoEsexo() {
+
+		new FuncionarioBuilder().comDataNascimento(20, 2, 2000).comSexo(Sexo.M).comTelefone("2659-0000")
+				.comJornadaDeTrabalho(jornadaTrabalho).comEmail("teste@teste.com").estaAtivo(true).comNome("Paulo")
+				.builder();
+
+	}
+
 //	@Test
 //	public void cadastraFuncionarioEJornadaNoBancoDeDados() {
 //
-//		Funcionario funcionaria = new FuncionarioBuilder().comNome("Mariana Laura Rosa Galvão")
-//				.comCpf("19532110542")
-//				.comDataNascimento(12, 05, 1997)
-//				.comSexo(Sexo.F)
+//		Funcionario funcionario = new FuncionarioBuilder().comNome("Michael Santos")
+//				.comCpf("63873882108")
+//				.comDataNascimento(20, 02, 1995)
+//				.comSexo(Sexo.M)
 //				.comTelefone("6228971430")
 //				.estaAtivo(true)
-//				.comEmail("marianalaurarosagalvao@teste.com").builder();
+//				.comEmail("michael.shel96@gmail.com").builder();
 //
-//		funcionarioRepository.save(funcionaria);
+//		funcionarioRepository.save(funcionario);
 //		
 //		JornadaTrabalho jornadaDeTrabalhoJoana = new JornadaTrabalhoBuilder()
 //			.comTipoTrabalhador(TipoTrabalhador.URBANO)
 //			.comCargaHorariaSemanal(44)
 //			.comIntervaloOptional(60)
 //			.comInicioJornada(8, 00)
-//			.comTerminoJornada(17, 00)
-//			.comFuncionario(funcionaria)
-//			.comSabado()
+//			.comTerminoJornada(18, 00)
+//			.comFuncionario(funcionario)
 //			.builder();
 //		
 //		jornadaTrabalhoRepository.save(jornadaDeTrabalhoJoana);
 //		
 //	}
-	
+
 //	@Test
 //	public void cadastraPonto() {
 //		
@@ -130,53 +116,41 @@ public class FuncionarioTest {
 //		
 //	}
 
-//	@Test
-//	public void testePontoDoBancoDeDadosSelect() {
-//		Optional<PontoEletronico> pontoJoanaDoBanco = pontoEletronicoRepository.findById((long) 1);
-//		
-//		System.out.println(pontoJoanaDoBanco.get().getInicioJornada());
-//		
-//		System.out.println(pontoJoanaDoBanco.get().tempoDeAtrasadoInicioJornada());
-//		
-//	}
-	
 	@Test
-	public void insereIntervalo() {
-		
-		Funcionario findByCpf = funcionarioRepository.findByCpf("30083209450");
+	public void testePontoDoBancoDeDadosSelect() {
+		Optional<PontoEletronico> pontoJoanaDoBanco = pontoEletronicoRepository.findById((long) 1);
+		System.out.println(pontoJoanaDoBanco.get().getInicioJornada());
 
-		PontoEletronico pontoJoana = pontoEletronicoRepository.findById(findByCpf.getId()).get();
-		
-//		PontoEletronico pontoJoanaDoBanco = pontoEletronicoRepository.findById((long) 1).get();
-		
-		
-		System.out.println(pontoJoana.tempoPermanenciaJornadaDiaria());
-		System.out.println(pontoJoana.tempoSaidaAntecipada());
-		System.out.println(pontoJoana.tempoPermanenciaComDescontoIntervalo());
-//		pontoJoana.baterCartao();
-//		
-//		pontoEletronicoRepository.save(pontoJoana);
-		
-		
-//		pontoJoana.validaInicioJornada();
-//		pontoJoana.validaInicioIntervalo();
-//		pontoJoana.validaTerminoIntervalo();
-		
-//		pontoEletronicoRepository.save(pontoJoana);
-		
+		System.out.println(pontoJoanaDoBanco.get().tempoDeAtrasadoInicioJornada());
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
-	
-	
+	// cpf 19532110542, 30083209450
+	@Test
+	public void testaBateCartaoFuncionario() {
+
+		Optional<Funcionario> funcionario = funcionarioRepository.findByCpf("63873882108");
+
+		if (funcionario.isPresent()) {
+			Optional<PontoEletronico> ponto = pontoEletronicoRepository
+					.findByPontoAtualDoFuncionario(funcionario.get());
+
+			if (ponto.isPresent()) {
+				ponto.get().bateCartao();
+				pontoEletronicoRepository.save(ponto.get());
+			
+			} else {
+				PontoEletronico newPonto = new PontoEletronicoBuilder()
+						.comFuncionario(funcionario.get())
+						.buider();
+				
+				newPonto.bateCartao();
+				pontoEletronicoRepository.save(newPonto);
+				
+			}
+			
+		}
+
+	}
+
 }
